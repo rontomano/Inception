0579bdc2b6e54010922da64afc8c79745b9e8572
rontomano <rontomeromano@gmail.com>
2023-11-07 09:25:19 +0100

    bonus working. Need ftp

--- srcs/requirements/wordpress/tools/config-wordpress.sh
+++ srcs/requirements/wordpress/tools/config-wordpress.sh
@@ -22,14 +22,33 @@ wp-cli.phar core install		\
 	--title=${WP_SITE_TITLE}    \
 	--admin_user=${WP_ADMIN_USER}     \
 	--admin_password=${WP_ADMIN_PASS} \
-	--admin_email=${WP_ADMIN_EMAIL}
+	--admin_email=${WP_ADMIN_EMAIL} \
+	--skip-email
 # Create user
 wp-cli.phar user create \
 	--allow-root \
 	--path='/var/www/wordpress' \
 	${WP_USER} \
 	${WP_USER_MAIL} \
-	--user_pass=${WP_USER_PASS}
+	--user_pass=${WP_USER_PASS} \
+
+# moving to path and config Redis
+cd /var/www/wordpress
+wp-cli.phar config set WP_REDIS_HOST redis --allow-root #I put --allowroot because i am on the root user on my VM
+wp-cli.phar config set WP_REDIS_PORT 6379 --raw --allow-root
+wp-cli.phar config set WP_CACHE_KEY_SALT $DOMAIN_NAME --allow-root
+# wp-cli.phar config set WP_REDIS_PASSWORD $REDIS_PASSWORD --allow-root
+wp-cli.phar config set WP_REDIS_CLIENT phpredis --allow-root
+wp-cli.phar plugin install redis-cache --activate --allow-root
+wp-cli.phar plugin update --all --allow-root
+wp-cli.phar redis enable --allow-root
+
+
+wp-cli.phar config set WP_DEBUG true --allow-root
+wp-cli.phar config set WP_DEBUG_LOG true --allow-root
+wp-cli.phar config set WP_DEBUG_DISPLAY false --allow-root
+
+#Done
 echo "Wordpress is configured"
 fi
 exec "$@"
